0191f420c113298364f77d8d52302c4ace7f08d0
Khaetneth <mrkhaetneth@gmail.com>
2026-09-04 03:26:17 +0700

    Drew Player Sprite

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import pygame
 
 from constants import *
 from logger import log_state
+from player import Player
 
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -24,6 +27,7 @@ def main():
                 return 
         
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
