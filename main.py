1f6605889bf943f43af5848d88920fe8f1122ab5
Khaetneth <mrkhaetneth@gmail.com>
2026-09-04 02:54:27 +0700

    Completed Chapter 2

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    dt = 0.0
+    clock = pygame.time.Clock()
+    
     # Drawing game
     while True:
         log_state()
@@ -22,6 +25,7 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
